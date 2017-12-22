61e367e2a1d24b3c307e9a8ba00c09bfa9f6f24a
Denis Sologub <contedevel2010@gmail.com>
2017-12-22 17:08:24 +0300

    Adds rules.

--- rule.cpp
+++ rule.cpp
@@ -4,7 +4,37 @@
  ***********************************************************************/
 #include "rule.h"
 
-rule::rule()
+rule::rule(const marks_t &&conditions, const marks_t &&outputs)
+    : m_conditions{std::make_unique<marks_t>(std::move(conditions))},
+      m_outputs{std::make_unique<marks_t>(std::move(outputs))}
 {
 
+}
+
+rule::~rule()
+{
+    m_conditions->clear();
+}
+
+bool rule::check(marks_t &marks, marks_t &outputs)
+{
+    if (marks.size() != m_conditions->size()
+            || outputs.size() != m_outputs->size()) {
+        return false;
+    }
+
+    for (auto s = begin(*m_conditions), d = begin(marks);
+         s != end(*m_conditions); ++s, ++d) {
+
+        if ((*s) != (*d)) {
+            return false;
+        }
+    }
+
+    for (auto s = begin(*m_outputs), d = begin(outputs);
+         s != end(*m_outputs); ++s, ++d) {
+        *d = (*s);
+    }
+
+    return true;
 }
--- rule.h
+++ rule.h
@@ -5,11 +5,20 @@
 #ifndef RULE_H
 #define RULE_H
 
+#include <memory>
+#include <vector>
+#include "emark.h"
+
+typedef std::vector<emark> marks_t;
 
 class rule
 {
+    std::unique_ptr<marks_t> m_conditions;
+    std::unique_ptr<marks_t> m_outputs;
 public:
-    rule();
+    rule(const marks_t &&conditions, const marks_t &&outputs);
+    ~rule();
+    bool check(marks_t &marks, marks_t &outputs);
 };
 
 #endif // RULE_H
